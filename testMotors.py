86d0b7d2160b46649bc360d869eff96da0b397b2
ArtificialOne <91615960+ArtificialOne@users.noreply.github.com>
2021-10-23 15:44:15 -0400

    Update testMotors.py

--- testMotors.py
+++ testMotors.py
@@ -15,6 +15,6 @@ atexit.register(turn_off_motors)
 lm.setSpeed(150) #Little over Half of 255 max speed
 rm.setSpeed(150)
 
-lm.run(Raspi_MotorHAT.FORWARD)
-rm.run(Raspi_MotorHAT.FORWARD)
+lm.run(Raspi_MotorHAT.FORWARD) #Placing OR  of these .BACKWARD will turn Robot 
+rm.run(Raspi_MotorHAT.FORWARD) #Placing OR  of these .BACKWARD will turn Robot
 time.sleep(1) #Disabled after 1 second
