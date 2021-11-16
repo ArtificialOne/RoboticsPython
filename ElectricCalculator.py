6638bb4a37292dc303b220bcdf4891237e57a9c6
ArtificialOne <91615960+ArtificialOne@users.noreply.github.com>
2021-11-15 21:56:21 -0500

    Update ElectricCalculator.py

--- ElectricCalculator.py
+++ ElectricCalculator.py
@@ -3,37 +3,44 @@ while True:
     print("2. Calculate Resistance (Voltage / Current)")
     print("3. Calculate Current (Voltage / Resistance)")
     print("4. Calculate Wattage (Voltage * Current)")
-    print("5. Exit Program")
+    print("5. Calculate Milliamps (Amps * 1000)")
+    print("6. Exit Program")
     number = input("> ")
     if (number == "1"):
-        print("- Calculating Voltage -")
+        print("- Identifying Voltage -")
         i = float(input("Current: "))
         res = float(input("Resistence: "))
         volt = i * res
         print(f"Voltage =  {volt:.2f}")
 
     elif(number == "2"):
-        print("- Calculating Resistance -")
+        print("- Identifying Resistance -")
         volt = float(input("Voltage: "))
         i = float(input("Current: "))
         res = volt / i
         print(f"Resistance = {res:.2f}")
 
     elif(number == "3"):
-        print("- Calculating Current -")
+        print("- Identifying Current -")
         volt = float(input("Voltage: "))
         res = float(input("Resistence: "))
         i = volt / res
         print(f"Current = {i:.2f}")
 
     elif(number == "4"):
-        print("- Calculating Wattage -")
+        print("- Identifying Wattage -")
         volt = float(input("Voltage: "))
         i = float(input("Current: "))
         watt = volt * i
         print(f"Wattage = {watt:.2f}")
 
     elif(number == "5"):
+        print("- Identifying Milliamps -")
+        amps = float(input("Current: "))
+        miliamp = amps * 1000
+        print(f"Milliamps: {miliamp:.2f}")
+
+    elif(number == "6"):
         break
     else:
         print("Not an option, Try again")
